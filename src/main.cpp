a30d4f71e0df47b120be786d377b9f6f3a3c169e
Upasana Porwal <upasanaporwal8@gmail.com>
2026-02-10 09:40:58 +0530

    add adafruit/Adafruit GFX,SSD1306 library done

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC,OLED_ADDR)) {
+        Serial.println("OLED not found");
+        while (true);
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+     display.println("Arduino UNO R4");
+     display.println("OLED with I2C");
+    display.println("Hello Upasana and Vanshika");
+    display.println("😄😄😄");
+    display.display();
 // write your initialization code here
 }
 
